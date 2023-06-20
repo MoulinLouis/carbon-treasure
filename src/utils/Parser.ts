d34f7a2c453a0867e97bd94ab8dcf9f43544f287
MoulinLouis <moulin.louis93@gmail.com>
2023-06-20 13:24:04 +0000

    feat(parser): add parseInput and formatOutput methods

---
+++ src/utils/Parser.ts
@@ -0,0 +1,80 @@
+import { Adventurer } from "../models/Adventurer";
+import { Area } from "../models/Area";
+import { Mountain } from "../models/Moutain";
+import { Treasure } from "../models/Treasure";
+
+export class Parser {
+  public static parseInputData(inputData: string): {
+    area: Area;
+    adventurers: Adventurer[];
+  } {
+    const lines = inputData.split("\n");
+
+    let area: Area | null = null;
+    const adventurers: Adventurer[] = [];
+
+    lines.forEach((line) => {
+      const parts = line.split(" - ");
+
+      switch (parts[0]) {
+        case "C":
+          area = new Area(parseInt(parts[1]), parseInt(parts[2]));
+          break;
+        case "M":
+          area?.addMountain(parseInt(parts[1]), parseInt(parts[2]));
+          break;
+        case "T":
+          area?.addTreasure(
+            parseInt(parts[1]),
+            parseInt(parts[2]),
+            parseInt(parts[3])
+          );
+          break;
+        case "A":
+          const adventurer = new Adventurer(
+            parts[1],
+            parseInt(parts[2]),
+            parseInt(parts[3]),
+            parts[4],
+            parts[5]
+          );
+          adventurer.area = area;
+          adventurers.push(adventurer);
+          break;
+        default:
+          console.log(`Unrecognized line: ${line}`);
+      }
+    });
+
+    if (!area) {
+      throw new Error("Area data missing in input data");
+    }
+
+    return { area, adventurers };
+  }
+
+  static formatOutputData(area: Area, adventurers: Adventurer[]): string {
+    let outputData = `C - ${area.width} - ${area.height}\n`;
+
+    area.grid.forEach((row) => {
+      row.forEach((cell) => {
+        if (cell.type === "MOUNTAIN") {
+          outputData += `M - ${cell.y} - ${cell.x}\n`;
+        }
+        if (
+          cell.type === "TREASURE" &&
+          cell.content !== null &&
+          cell.content instanceof Treasure
+        ) {
+          outputData += `T - ${cell.y} - ${cell.x} - ${cell.content.amount}\n`;
+        }
+      });
+    });
+
+    adventurers.forEach((adventurer) => {
+      outputData += `A - ${adventurer.name} - ${adventurer.horizontalPosition} - ${adventurer.verticalPosition} - ${adventurer.orientation} - ${adventurer.treasuresCollected}\n`;
+    });
+
+    return outputData;
+  }
+}
