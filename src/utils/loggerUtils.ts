d801626f13bcf1c1aa14e5ae8947c1940a93795d
MoulinLouis <moulin.louis93@gmail.com>
2023-06-22 12:52:24 +0000

    chore(test): ignore log write method for coverage

--- src/utils/loggerUtils.ts
+++ src/utils/loggerUtils.ts
@@ -5,6 +5,7 @@ export class LoggerUtils {
   private static filePath = "./data/log.txt";
 
   public static write(message: string) {
+    /* istanbul ignore next */
     if (process.env.NODE_ENV !== "test") {
       const timestamp = new Date().toISOString();
       fs.appendFileSync(
