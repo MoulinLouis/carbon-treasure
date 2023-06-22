0affecb33dcff19a15b7b866c7404c3f21d02d63
MoulinLouis <moulin.louis93@gmail.com>
2023-06-22 12:37:22 +0000

    feat: handle errors in try catch

--- src/index.ts
+++ src/index.ts
@@ -8,24 +8,28 @@ const outputFilePath = "./data/output.txt";
 console.log(`Reading file from path ${inputFilePath}`);
 
 // Read the input file (./data/input.txt)
-const inputData = FileUtils.readFile(inputFilePath);
+try {
+  const inputData = FileUtils.readFile(inputFilePath);
 
-// Parse the input data
-const { area, adventurers } = ParserUtils.parseInputData(inputData);
+  // Parse the input data
+  const { area, adventurers } = ParserUtils.parseInputData(inputData);
 
-// Show the map in the console
-console.log("Displaying map:");
-console.log(area.displayMap());
+  // Show the map in the console
+  console.log("Displaying map:");
+  console.log(area.displayMap());
 
-// Add adventurers and execute their movements
-adventurers.forEach((adventurer) => {
-  adventurer.executeMovementSequence();
-});
+  // Add adventurers and execute their movements
+  adventurers.forEach((adventurer) => {
+    adventurer.executeMovementSequence();
+  });
 
-// Format output data
-const outputData = ParserUtils.formatOutputData(area, adventurers);
+  // Format output data
+  const outputData = ParserUtils.formatOutputData(area, adventurers);
 
-// Write the formatted result to the output file (./data/output.txt)
-FileUtils.writeFile(outputFilePath, outputData);
+  // Write the formatted result to the output file (./data/output.txt)
+  FileUtils.writeFile(outputFilePath, outputData);
 
-console.log(`Successfully wrote output file: ${outputFilePath}`);
+  console.log(`Successfully wrote output file: ${outputFilePath}`);
+} catch (err) {
+  console.error(`Unexpected error: ${err}`);
+}
